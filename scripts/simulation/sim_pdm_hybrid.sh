ca821bb027227ff7dbaf48e22c38aaf126b9fe24
Daniel Dauner <d.dauner@gmx.de>
2023-07-03 18:01:03 +0200

    fix typo

--- scripts/simulation/sim_pdm_hybrid.sh
+++ scripts/simulation/sim_pdm_hybrid.sh
@@ -5,8 +5,8 @@ CHECKPOINT=/path/to/checkpoint.ckpt
 
 python $NUPLAN_DEVKIT_ROOT/nuplan/planning/script/run_simulation.py \
 +simulation=$CHALLENGE \
-planner=pdm_hyrbid_planner \
-planner.pdm_hyrbid_planner.checkpoint_path=$CHECKPOINT \
+planner=pdm_hybrid_planner \
+planner.pdm_hybrid_planner.checkpoint_path=$CHECKPOINT \
 scenario_filter=$SPLIT \
 scenario_builder=nuplan \
 hydra.searchpath="[pkg://nuplan_garage.planning.script.config.common, pkg://nuplan_garage.planning.script.config.simulation, pkg://nuplan.planning.script.config.common, pkg://nuplan.planning.script.experiments]"
