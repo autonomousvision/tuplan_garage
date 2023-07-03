aff4d8f1e56ebc315af4c755638d60ac1142f2e4
Daniel Dauner <d.dauner@gmx.de>
2023-07-03 17:46:09 +0200

    update simulation scripts

---
+++ scripts/simulation/sim_pdm_hybrid.sh
@@ -0,0 +1,12 @@
+
+SPLIT=val14_split
+CHALLENGE=closed_loop_reactive_agents # open_loop_boxes, closed_loop_nonreactive_agents, closed_loop_reactive_agents
+CHECKPOINT=/path/to/checkpoint.ckpt
+
+python $NUPLAN_DEVKIT_ROOT/nuplan/planning/script/run_simulation.py \
++simulation=$CHALLENGE \
+planner=pdm_hyrbid_planner \
+planner.pdm_hyrbid_planner.checkpoint_path=$CHECKPOINT \
+scenario_filter=$SPLIT \
+scenario_builder=nuplan \
+hydra.searchpath="[pkg://nuplan_garage.planning.script.config.common, pkg://nuplan_garage.planning.script.config.simulation, pkg://nuplan.planning.script.config.common, pkg://nuplan.planning.script.experiments]"
