b3634bce5fc6c354f87f43387127d1b2a1827cf4
Daniel Dauner <d.dauner@gmx.de>
2023-10-16 18:08:28 +0200

    Add baseline training/sim configs

---
+++ scripts/training/train_plancnn.sh
@@ -0,0 +1,27 @@
+TRAIN_EPOCHS=100
+TRAIN_LR=1e-4
+TRAIN_LR_MILESTONES=[50,75]
+TRAIN_LR_DECAY=0.1
+BATCH_SIZE=64
+SEED=0
+
+JOB_NAME=training_plancnn
+CACHE_PATH=/path/to/cache/
+USE_CACHE_WITHOUT_DATASET=False
+
+python $NUPLAN_DEVKIT_ROOT/nuplan/planning/script/run_training.py \
+seed=$SEED \
+py_func=train \
++training=training_raster_model \
+objective=l1_objective \
+job_name=$JOB_NAME \
+scenario_builder=nuplan \
+cache.cache_path=$CACHE_PATH \
+cache.use_cache_without_dataset=$USE_CACHE_WITHOUT_DATASET \
+lightning.trainer.params.max_epochs=$TRAIN_EPOCHS \
+data_loader.params.batch_size=$BATCH_SIZE \
+optimizer.lr=$TRAIN_LR \
+lr_scheduler=multistep_lr \
+lr_scheduler.milestones=$TRAIN_LR_MILESTONES \
+lr_scheduler.gamma=$TRAIN_LR_DECAY \
+hydra.searchpath="[pkg://tuplan_garage.planning.script.config.common, pkg://tuplan_garage.planning.script.config.training, pkg://tuplan_garage.planning.script.experiments, pkg://nuplan.planning.script.config.common, pkg://nuplan.planning.script.experiments]"
